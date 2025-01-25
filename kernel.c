2849d06e73b9418cdd09d52a4d35da040b87f398
zll600 <3400692417@qq.com>
2025-01-25 11:19:33 +0800

    Kernel Panic

--- kernel.c
+++ kernel.c
@@ -31,11 +31,10 @@ void putchar(char ch) {
 }
 
 void kernel_main(void) {
-    printf("\n\nHello %s\n", "World!");
-    printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
-    for (;;) {
-        __asm__ __volatile__("wfi");
-    }
+    memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
+
+    PANIC("booted!");
+    printf("unreachable here!\n");
 }
 
 __attribute__((section(".text.boot")))
