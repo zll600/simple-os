07d74630315e6a02d0138bd9dc619bee47c30f9f
zll600 <3400692417@qq.com>
2025-01-25 18:52:46 +0800

    Application

--- run.sh
+++ run.sh
@@ -4,14 +4,21 @@ set -xue
 # QEMU file path
 QEMU=qemu-system-riscv32
 
+OBJCOPY=$(which llvm-objcopy)
+
 # Path to clang and compiler flags
-CC=clang
+CC=$(which clang)
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
+
+# Build the shell (application)
+$CC $CFLAGS -Wl,-Tuser.ld -Wl,-Map=shell.map -o shell.elf shell.c user.c common.c
+$OBJCOPY --set-section-flags .bss=alloc,contents -O binary shell.elf shell.bin
+$OBJCOPY -Ibinary -Oelf32-littleriscv shell.bin shell.bin.o
 
 # Build the kernel
 $CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
-	kernel.c common.c
+	kernel.c common.c shell.bin.o
 
 # Start QEMU
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
-	-kernel kernel.elf
+# $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+# 	-kernel kernel.elf
