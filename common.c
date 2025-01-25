0aab04c3fff4641069135fd81e64acdf28b40517
zll600 <3400692417@qq.com>
2025-01-25 11:04:51 +0800

    Printf Hello world

---
+++ common.c
@@ -0,0 +1,63 @@
+#include "common.h"
+
+void putchar(char ch);
+
+void printf(const char *fmt, ...) {
+    va_list vargs;
+    va_start(vargs, fmt);
+
+    while (*fmt) {
+        if (*fmt == '%') {
+            fmt++; // Skip '%'
+            switch (*fmt) { // Read the next character
+                case '\0': // '%' at the end of the format string
+                    putchar('%');
+                    goto end;
+                case '%': // Print '%'
+                    putchar('%');
+                    break;
+                case 's': { // Print a NULL-terminated string.
+                    const char *s = va_arg(vargs, const char *);
+                    while (*s) {
+                        putchar(*s);
+                        s++;
+                    }
+                    break;
+                }
+                case 'd': { // Print an integer in decimal.
+                    int value = va_arg(vargs, int);
+                    if (value < 0) {
+                        putchar('-');
+                        value = -value;
+                    }
+
+                    int divisor = 1;
+                    while (value / divisor > 9)
+                        divisor *= 10;
+
+                    while (divisor > 0) {
+                        putchar('0' + value / divisor);
+                        value %= divisor;
+                        divisor /= 10;
+                    }
+
+                    break;
+                }
+                case 'x': { // Print an integer in hexadecimal.
+                    int value = va_arg(vargs, int);
+                    for (int i = 7; i >= 0; i--) {
+                        int nibble = (value >> (i * 4)) & 0xf;
+                        putchar("0123456789abcdef"[nibble]);
+                    }
+                }
+            }
+        } else {
+            putchar(*fmt);
+        }
+
+        fmt++;
+    }
+
+end:
+    va_end(vargs);
+}
